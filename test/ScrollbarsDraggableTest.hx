f07ed36c0f1ec4cc3c2b7d02885332b969d10ac3
IBwWG <inthebeginningwasthewordgame@gmail.com>
2016-04-22 23:10:41 +0200

    In progress, on hold.

--- test/ScrollbarsDraggableTest.hx
+++ test/ScrollbarsDraggableTest.hx
@@ -1,43 +1,39 @@
 package ;
 
 import flixel.math.FlxRect;
+import flixel.util.FlxColor;
 import gimmicky.FlxScrollableArea;
 import massive.munit.util.Timer;
 import massive.munit.Assert;
 import massive.munit.async.AsyncFactory;
 import gimmicky.FlxScrollbar;
-import mockatoo.Mockatoo.*;
-using mockatoo.Mockatoo.*;
+import flixel.FlxTest;
+import mockatoo.Mockatoo;
+using mockatoo.Mockatoo;
 
-class ScrollbarsDraggableTest 
+/* Because of the way FlxScrollableArea is implemented, making its own two private scrollbars normally,
+ * we duplicate each of them because we are only interested in covering draw and update anyway.
+ */
+
+class ScrollbarsDraggableTest extends FlxTest implements mockatoo.MockTest
 {
-	var area:FlxScrollableArea; // TODO: mock out
+	var areaMock = Mockatoo.mock( FlxScrollableArea ); // um...https://github.com/misprintt/mockatoo/issues/56
+	//@:mock var areaMock:FlxScrollableArea;
 	var instance:FlxScrollbar; 
 	var randomInt:Int;
 	var squareViewPort:FlxRect;
 	var wideContent:FlxRect;
 	var tallContent:FlxRect;
 	
-	@BeforeClass
-	public function beforeClass():Void
-	{
-		area = new FlxScrollableArea( squareViewPort, tallContent, ResizeMode.FIT_WIDTH, randomInt );
-	}
-	
-	@AfterClass
-	public function afterClass():Void
-	{
-		area = null;
-	}
-	
 	@Before
 	public function setup():Void
 	{
-		randomInt = randomInt = Std.random( 89 ) + 1;
+		randomInt = Std.random( 89 ) + 1;
 		squareViewPort = new FlxRect( 0, 0, 100, 100 );
 		wideContent = new FlxRect( -500, -500, 400, 100 );
 		tallContent = new FlxRect( -500, -500, 100, 400 );
-		// instance = 
+		//areaMock = new FlxScrollableArea( squareViewPort, tallContent, ResizeMode.FIT_WIDTH );
+		//instance = new FlxScrollbar( 80, 0, 20, 100, FlxScrollbarOrientation.VERTICAL, FlxColor.LIME, areaMock, true );
 		destroyable = instance;
 		resetGame();
 	}
@@ -45,6 +41,7 @@ class ScrollbarsDraggableTest
 	@After
 	public function tearDown():Void
 	{
+		//areaMock = null;
 	}
 	
 	@Test
